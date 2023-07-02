df471fbbd9de343eef7821845fab138b00b917cc
lcai18 <lcai18@jh.edu>
2023-07-01 20:25:57 -0400

    auto scrolls down for each message input

--- frontend/chatbot/src/app/ChatMessageList.jsx
+++ frontend/chatbot/src/app/ChatMessageList.jsx
@@ -1,13 +1,24 @@
 import "./globals.css"
-import React from "react";
-
+import React, {useRef, useEffect} from "react";
 const ChatMessageList = ({ messages }) => {
+
+  const messageEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messageEndRef.current?.scrollIntoView({ behavior: "smooth" })
+  }
+
+  useEffect(() => {
+    scrollToBottom()
+  }, [messages]);
+
   return (
         <div className="chat-container">
             
         {messages.map((message, index) => (
             <div key={index} className="message-bubble">{message}</div>
         ))}
+        <div ref = {messageEndRef}/>
         </div>
   );
 };
