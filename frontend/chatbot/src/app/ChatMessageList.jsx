edda6d5bd699c233ca87ace566edea19e9524aa9
lcai18 <lcai18@jh.edu>
2023-07-01 21:56:31 -0400

    comments added

--- frontend/chatbot/src/app/ChatMessageList.jsx
+++ frontend/chatbot/src/app/ChatMessageList.jsx
@@ -3,22 +3,23 @@ import React, {useRef, useEffect} from "react";
 const ChatMessageList = ({ messages }) => {
 
   const messageEndRef = useRef(null);
-
+//scroll to the bottom after a new message enters
   const scrollToBottom = () => {
     messageEndRef.current?.scrollIntoView({ behavior: "smooth" })
   }
-
+//only re render if message added
   useEffect(() => {
     scrollToBottom()
   }, [messages]);
 
+
+{/* map each message to its index and print it out */}
   return (
     <div className="chat-container">
       <div className="chat-messages">
-        {messages.map((message, index) => (
+        {messages.map((message, index) => ( 
             <div key={index} className="message-bubble">{message}</div>
         ))}
-      
       <div ref = {messageEndRef}/>
       </div>
     </div>
--- frontend/chatbot/src/app/chat_section.tsx
+++ frontend/chatbot/src/app/chat_section.tsx
@@ -1,23 +1,33 @@
 "use client";
-import React, { useState } from "react";
-import ChatMessageList from "./ChatMessageList";
+import React, { useState, useEffect, useRef } from "react";
 import QueryInput from "./querying";
+import ChatMessageList from "./ChatMessageList";
+import ResetChat from "./ResetChat";
 
 const ChatBot = () => {
   const [chatMessages, setChatMessages] = useState<string[]>([]);
+
   const handleQuerySubmit = (query: string) => {
     // Perform actions or logic with the submitted query
     // For example, send the query to an API, update the chat messages, etc.
     console.log("Submitted query:", query);
 
-    setChatMessages((prevMessages) => prevMessages.concat(query));
+    // Update the chat messages with the new query
+    setChatMessages((prevMessages) => [...prevMessages, query]);
+  };
+
+  const handleResetChat = () => {
+    //reset chat box
+    setChatMessages([]);
   };
 
   return (
     <div>
-      <ChatMessageList messages={chatMessages} />
+      <ChatMessageList messages={chatMessages} /> {/* chat messages */}
       {/* Other chatbot components */}
-      <QueryInput onQuerySubmit={handleQuerySubmit} />
+      <QueryInput onQuerySubmit={handleQuerySubmit} />{" "}
+      {/* make request to respond to query */}
+      <ResetChat onResetChat={handleResetChat} />
       {/* Other chatbot components */}
     </div>
   );
