af978e2db5f59884f8c0faebedb57c63a63e7bff
lcai18 <123596664+lcai18@users.noreply.github.com>
2023-07-26 22:28:01 -0400

    Delete settings.py

--- backend/settings.py
+++
@@ -1,9 +0,0 @@
-# settings.py
-OPENAI_API_KEY = "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"
-
-CLICKHOUSE_CONNECTION_DETAILS = {
-    "host": "msc-a8111894.us-east-1.aws.myscale.com",
-    "port": 443,
-    "username": "larrycai99",
-    "password": "passwd_3GLKeGrPucrsAN"
-}
